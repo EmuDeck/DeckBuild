0251582fa8c28aec4ce842b1075cb587b4041287
Witherking25 <witherking@withertech.com>
2022-11-08 21:55:26 -0500

    test: add dummy test to shut it up

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,1 +1,9 @@
 
+describe('index', () => {
+	describe('dummy', () => {
+		it('should return a string containing the message', () => {
+			const message = 'Hello';
+			expect(message).toMatch(message);
+		});
+	});
+});
