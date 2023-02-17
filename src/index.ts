c71657be4c3145611fd3689b761d41fcae0d36d6
Witherking25 <witherking@withertech.com>
2023-02-17 17:51:10 -0500

    feat: rename build task to package and add build task that creates a folder as opposed to a zip
    a large refactoring
    
    BREAKING CHANGE: This renames dbuild build to dbuild package. dbuild build is a different task now
    that does a different thing

--- src/index.ts
+++ src/index.ts
@@ -28,8 +28,22 @@ void yargs
 	.command<{ dev: boolean }>({
 	command: 'build',
 	aliases: ['b'],
-	describe: 'Builds a plugin and creates a zip file for distribution',
+	describe: 'Builds a plugin',
 	handler: build,
+	builder: {
+		'dev': {
+			alias: 'd',
+			describe: 'Makes a dev build',
+			type: 'boolean',
+			default: false,
+		}
+	}
+})
+	.command<{ dev: boolean }>({
+	command: 'package',
+	aliases: ['p'],
+	describe: 'Builds a plugin and creates a zip file for distribution',
+	handler: _package,
 	builder: {
 		'dev': {
 			alias: 'd',
@@ -91,7 +105,7 @@ function build(args: yargs.ArgumentsCamelCase<{ dev: boolean }>)
 	} else throw new Error('podman or builder not found')
 
 	//pull builder images
-	child_process.execSync(`${container} pull ghcr.io/emudeck/builder:latest`)
+	child_process.execSync(`${container} pull ghcr.io/steamdeckhomebrew/builder:latest`)
 	child_process.execSync(`${container} pull ghcr.io/steamdeckhomebrew/holo-base:latest`)
 
 	//backend
@@ -145,7 +159,161 @@ function build(args: yargs.ArgumentsCamelCase<{ dev: boolean }>)
 		console.log(`Plugin ${plugin.name} does not have a backend`)
 	}
 	//frontend
-	child_process.execSync(`${container} run --rm -i -e NODE_ENV="${args.dev ? 'development' : 'production'}" -v "${process.cwd()}":/plugin -v "${path.join(process.cwd(), 'build', plugin.name)}":/out ghcr.io/emudeck/builder:latest`)
+	child_process.execSync(`${container} run --rm -i -e NODE_ENV="${args.dev ? 'development' : 'production'}" -v "${process.cwd()}":/plugin -v "${path.join(process.cwd(), 'build', plugin.name)}":/out ghcr.io/steamdeckhomebrew/builder:latest`)
+	console.log(` Built ${plugin.name} frontend`)
+
+	//zip
+	const output = `${plugin.name}-${package_.version}${args.dev ? '-dev' : ''}`;
+	const license = (fs.existsSync(path.join(process.cwd(), 'LICENSE')) ? 'LICENSE':fs.existsSync(path.join(process.cwd(), 'license')) ? 'license':fs.existsSync(path.join(process.cwd(), 'LICENSE.md')) ? 'LICENSE.md':fs.existsSync(path.join(process.cwd(), 'license.md')) ? 'license.md':undefined)
+	const readme = (fs.existsSync(path.join(process.cwd(), 'README.md')) ? 'README.md':fs.existsSync(path.join(process.cwd(), 'readme.md')) ? 'readme.md':undefined)
+	const has_python = glob.sync(path.join(process.cwd(), '*.py')).length > 0
+	const has_bin = fs.existsSync(path.join(process.cwd(), 'build', plugin.name, 'bin'))
+	const has_defaults = fs.existsSync(path.join(process.cwd(), 'defaults'))
+
+	process.chdir('build')
+	fs.mkdirSync(path.join(process.cwd(), output, plugin.name), {recursive: true})
+	const entries = [
+		'dist',
+		'plugin.json',
+		'package.json'
+	]
+
+	if (has_bin)
+	{
+		entries.push('bin')
+	}
+
+	if (has_python)
+	{
+		entries.push(...glob.sync(`${plugin.name}/*.py`, {nodir: true}))
+	}
+
+	if (has_defaults)
+	{
+		if (!fs.existsSync(path.join(plugin.name, 'defaults', 'defaults.txt')))
+		{
+			const defaults = fs.opendirSync(path.join(plugin.name, 'defaults'))
+			for (const name in defaults)
+			{
+				fs.copySync(path.join(plugin.name, 'defaults', name), path.join(plugin.name, name), {
+					recursive: true
+				});
+				entries.push(path.join(plugin.name, name))
+			}
+		} else
+		{
+			if (!package_.name.includes('plugin-template'))
+			{
+				throw new Error('defaults.txt found in defaults folder, please remove either defaults.txt or the defaults folder.')
+			} else
+			{
+				console.log('plugin template, allowing defaults.txt')
+			}
+		}
+	}
+
+	if (license)
+	{
+		entries.push(path.join(plugin.name, license))
+	}
+
+	if (readme)
+	{
+		entries.push(path.join(plugin.name, readme))
+	}
+
+	for(const entry of entries)
+	{
+		fs.copySync(path.join(process.cwd(), plugin.name, entry), path.join(process.cwd(), output, plugin.name), {
+			recursive: true
+		})
+	}
+	fs.rmdirSync(plugin.name)
+	process.chdir('..')
+}
+
+function _package(args: yargs.ArgumentsCamelCase<{ dev: boolean }>)
+{
+	if (fs.existsSync(path.join(process.cwd(), 'build')))
+	{
+		fs.rmdirSync(path.join(process.cwd(), 'build'), {
+			recursive: true
+		});
+		fs.mkdirSync(path.join(process.cwd(), 'build', plugin.name, 'dist'), {
+			recursive: true
+		});
+	} else
+	{
+		fs.mkdirSync(path.join(process.cwd(), 'build', plugin.name, 'dist'), {
+			recursive: true
+		});
+	}
+	let container: string;
+
+	if (fs.existsSync('/var/run/builder.pid'))
+	{
+		container = 'builder';
+	} else if (fs.existsSync('/usr/bin/podman') && fs.existsSync('/usr/bin/slirp4netns') && fs.existsSync('/usr/bin/fuse-overlayfs'))
+	{
+		container = 'podman'
+	} else throw new Error('podman or builder not found')
+
+	//pull builder images
+	child_process.execSync(`${container} pull ghcr.io/steamdeckhomebrew/builder:latest`)
+	child_process.execSync(`${container} pull ghcr.io/steamdeckhomebrew/holo-base:latest`)
+
+	//backend
+	console.log(`Detecting backend for plugin ${plugin.name}`);
+	const docker_name = `backend-${plugin.name.toLowerCase().replace(' ', '-')}`;
+	const dockerfile_exists = fs.existsSync(path.join(process.cwd(), 'backend', 'Dockerfile'));
+	const entrypoint_exists = fs.existsSync(path.join(process.cwd(), 'backend', 'entrypoint.sh'));
+	if (dockerfile_exists)
+	{
+		console.log('Grabbing provided dockerfile.');
+		console.log('Building provided Dockerfile.');
+		child_process.execSync(`${container} build -f ${path.join(process.cwd(), 'backend', 'Dockerfile')} -t "${docker_name}" .`)
+		fs.mkdirSync(path.join(process.cwd(), 'build', 'backend', 'out'), {
+			recursive: true
+		})
+		if (entrypoint_exists)
+		{
+			console.log(`Running docker image "${docker_name}" with provided entrypoint script.`)
+			child_process.execSync(`${container} run --rm -i -v "${path.join(process.cwd(), 'backend')}":/backend -v "${path.join(process.cwd(), 'build', 'backend', 'out')}":/backend/out --entrypoint /backend/entrypoint.sh "${docker_name}"`)
+		} else
+		{
+			console.log(`Running docker image "${docker_name}" with entrypoint script specified in Dockerfile.`)
+			child_process.execSync(`${container} run --rm -i -v "${path.join(process.cwd(), 'backend')}":/backend -v "${path.join(process.cwd(), 'build', 'backend', 'out')}":/backend/out "${docker_name}"`)
+		}
+		fs.mkdirSync(path.join(process.cwd(), 'build', plugin.name, 'bin'), {
+			recursive: true
+		})
+		fs.copySync(path.join(process.cwd(), 'build', 'backend', 'out'), path.join(process.cwd(), 'build', plugin.name, 'bin'), {
+			recursive: true,
+			overwrite: true
+		})
+		child_process.execSync(`${container} image rm "${docker_name}"`)
+		console.log(`Built ${plugin.name} backend`)
+	} else if (!dockerfile_exists && entrypoint_exists)
+	{
+		console.log('Grabbing default builder image and using provided entrypoint script.')
+		fs.mkdirSync(path.join(process.cwd(), 'build', 'backend', 'out'), {
+			recursive: true
+		})
+		child_process.execSync(`${container} run --rm -i -v "${path.join(process.cwd(), 'backend')}":/backend -v "${path.join(process.cwd(), 'build', 'backend', 'out')}":/backend/out ghcr.io/steamdeckhomebrew/holo-base:latest`)
+		fs.mkdirSync(path.join(process.cwd(), 'build', plugin.name, 'bin'), {
+			recursive: true
+		})
+		fs.copySync(path.join(process.cwd(), 'build', 'backend', 'out'), path.join(process.cwd(), 'build', plugin.name, 'bin'), {
+			recursive: true,
+			overwrite: true
+		})
+		console.log(`Built ${plugin.name} backend`)
+	} else
+	{
+		console.log(`Plugin ${plugin.name} does not have a backend`)
+	}
+	//frontend
+	child_process.execSync(`${container} run --rm -i -e NODE_ENV="${args.dev ? 'development' : 'production'}" -v "${process.cwd()}":/plugin -v "${path.join(process.cwd(), 'build', plugin.name)}":/out ghcr.io/steamdeckhomebrew/builder:latest`)
 	console.log(` Built ${plugin.name} frontend`)
 
 	//zip
@@ -216,11 +384,10 @@ function deploy(args: yargs.ArgumentsCamelCase<{ reload: boolean, dev: boolean }
 		deck = new Deck(require(path.join(process.cwd(), '.vscode', 'settings.json')));
 	} else throw new Error(`${path.join(process.cwd(), 'deck.json')} or ${path.join(process.cwd(), '.vscode', 'settings.json')} does not exist`);
 	build(args)
-	const zip = `${plugin.name}-${package_.version}${args.dev ? '-dev' : ''}.zip`;
-	child_process.execSync(`unzip "${path.join(process.cwd(), 'build', zip)}" -d ${path.join(process.cwd(), 'build', 'deploy')}`)
+	const deploy = path.join(process.cwd(), 'build', `${plugin.name}-${package_.version}${args.dev ? '-dev' : ''}`);
 	child_process.execSync(`ssh deck@${deck.deckip} -p ${deck.deckport} ${deck.deckkey.replace('$HOME', process.env.HOME ? process.env.HOME:'')} 'mkdir -p ${deck.deckdir}/homebrew/pluginloader && mkdir -p ${deck.deckdir}/homebrew/plugins'`)
 	child_process.execSync(`ssh deck@${deck.deckip} -p ${deck.deckport} ${deck.deckkey.replace('$HOME', process.env.HOME ? process.env.HOME:'')} 'echo "${deck.deckpass}" | sudo -S chmod -R ug+rw ${deck.deckdir}/homebrew/'`)
-	child_process.execSync(`rsync -azp --delete --chmod=D0755,F0755 --rsh='ssh -p ${deck.deckport} ${deck.deckkey.replace('$HOME', process.env.HOME ? process.env.HOME:'')}' "${path.join(process.cwd(), 'build', 'deploy', plugin.name)}" deck@${deck.deckip}:${deck.deckdir}/homebrew/plugins`)
+	child_process.execSync(`rsync -azp --delete --chmod=D0755,F0755 --rsh='ssh -p ${deck.deckport} ${deck.deckkey.replace('$HOME', process.env.HOME ? process.env.HOME:'')}' "${path.join(deploy, plugin.name)}" deck@${deck.deckip}:${deck.deckdir}/homebrew/plugins`)
 	if (args.reload)
 	{
 		child_process.execSync(`ssh deck@${deck.deckip} -p ${deck.deckport} ${deck.deckkey.replace('$HOME', process.env.HOME ? process.env.HOME:'')} 'echo "${deck.deckpass}" | sudo -S systemctl restart plugin_loader.service'`)
